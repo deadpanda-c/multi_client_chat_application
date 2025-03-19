3958ad4bba837698a6f8cd7eebdf9e8930eb65b0
Clément Lagier <clement.lagier@epitech.eu>
2025-03-19 16:39:20 +0900

    [UPDATE] Added some macros for the ServerException class

--- include/Server.hpp
+++ include/Server.hpp
@@ -10,12 +10,16 @@
 #include <arpa/inet.h>
 #include <sys/select.h>
 
-#include "Client.hpp"
-
 #define MAX_CLIENTS 10
 #define MAX_BUFFER_SIZE 1024
 #define SOCKET_CREATION_FAILED "Failed to create socket"
 #define SOCKET_BIND_FAILED "Failed to bind socket"
+#define SOCKET_LISTEN_FAILED "Failed to listen on socket"
+#define SOCKET_ACCEPT_FAILED "Failed to accept connection"
+#define SELECT_FAILED "Failed to select"
+#define SERVER_NOT_RUNNING "Server is not running"
+#define INVALID_CLIENT_FD "Invalid client file descriptor"
+#define SOCKET_FD_IN_CLIENTS "Socket file descriptor in client"
 
 class Server {
 public:
@@ -30,29 +34,38 @@ public:
     };
 
     Server();
-    Server(unsigned int port);
+    Server(unsigned short port);
     ~Server();
 
     void init();
     void run();
     void stop();
 
-    void addClient(std::shared_ptr<Client> client);
-    void removeClient(std::shared_ptr<Client> client);
+    void readFromClients();
+    void writeToClients();
+
+    void addClient(int client);
+    void removeClient(int client);
 
     void broadcast(const std::string& message);
 
 private:
-    std::vector<std::shared_ptr<Client>> _clients;
+    void _initFdSets();
+
+    std::vector<int> _clients;
     int _socket;
     int _maxClients;
-    unsigned _port;
+    unsigned short _port;
     int _serverSocket;
     size_t _maxFd;
 
     fd_set _writeFds;
     fd_set _readFds;
+    fd_set _exceptFds;
 
     struct sockaddr_in _serverAddr;
+    struct sockaddr_in _clientAddr;
+    socklen_t _clientAddrLen;
+
     bool _running;
 };
