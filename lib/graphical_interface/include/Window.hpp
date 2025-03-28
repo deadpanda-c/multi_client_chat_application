5e16c880e8df934c08755e41605411fb40177512
Clément Lagier <clement.lagier@epitech.eu>
2025-03-28 22:39:41 +0900

    [ADD] Added a side menu with the name of the differents connected users

--- lib/graphical_interface/include/Window.hpp
+++ lib/graphical_interface/include/Window.hpp
@@ -1,12 +1,22 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
+#include <QListWidget>
 #include <QApplication>
 #include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTextEdit>
 
 #define WINDOW_WIDTH 1000
 #define WINDOW_HEIGHT 800
+
+#define USER_NAME_FONT QFont("Arial", 14)
 
 class Window : public QWidget {
   public:
@@ -14,16 +24,50 @@ class Window : public QWidget {
     {
       setWindowTitle(title);
       setMinimumSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+      // main layout
+      mainLayout = new QHBoxLayout(this);
+
+      // side menu
+      sideMenu = new QListWidget(this);
+      sideMenu->setFixedWidth(300);
+      sideMenu->setFixedHeight(WINDOW_HEIGHT);
+
+      addItemToSideMenu("Toto");
+      addItemToSideMenu("Tata");
+      addItemToSideMenu("Titi");
+      addItemToSideMenu("Tutu");
+      addItemToSideMenu("Tete");
+
+
+      chat = new QListWidget(this);
+      chat->setFixedWidth(WINDOW_WIDTH - 300);
+      chat->setFixedHeight(WINDOW_HEIGHT);
+
+
+      mainLayout->addWidget(sideMenu);
+      mainLayout->addWidget(chat);
     }
+
     ~Window();
+
+    void addItemToSideMenu(const QString &item)
+    {
+      QListWidgetItem *newItem = new QListWidgetItem(item);
+      newItem->setFont(USER_NAME_FONT);
+      sideMenu->addItem(newItem);
+    }
 
     void show()
     {
       QWidget::show();
     }
   private:
+    QListWidget *sideMenu;
+    QListWidget *chat;
 
+    QHBoxLayout *mainLayout;
+    QTextEdit *textEdit;
 
-
-
+    std::vector<QWidget*> widgets;
 };
