6eb368dc691abc29b7f87b21e87c83a39c48bd18
Clément Lagier <clement.lagier@epitech.eu>
2025-03-29 19:05:17 +0900

    [UPDATE] Initialize the Qt interface directly in the main function

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,15 +1,14 @@
 #include "Client.hpp"
 
-
-int main(int ac, char **av) {
+int main(int ac, char **av)
+{
   unsigned short port = (ac > 1) ? std::stoi(av[2]) : 8080;
   std::string ip = (ac > 2) ? av[1] : "127.0.0.1";
 
-  try {
-    Client client(ip, port);
-    client.run();
-  } catch (std::exception &e) {
-    std::cerr << e.what() << std::endl;
-  }
-  return 0;
+  QApplication app(ac, av);
+  Client client(ip, port, "My Window");
+
+  client.show();
+  return app.exec();
 }
+
