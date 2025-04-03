048b9518b67a30c417d3d244366c8b0ceae84e39
Clément Lagier <clement.lagier@epitech.eu>
2025-04-03 15:46:35 +0900

    [FIX] Handle client error when no args is set

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,6 +2,11 @@
 
 int main(int ac, char **av)
 {
+  if (ac < 2 || ac > 3)
+  {
+    std::cerr << "Usage: " << av[0] << " <ip> [port]" << std::endl;
+    return 1;
+  }
   unsigned short port = (ac > 1) ? std::stoi(av[2]) : 8080;
   std::string ip = (ac > 2) ? av[1] : "127.0.0.1";
 
