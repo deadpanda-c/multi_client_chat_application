9567d0c55552bbeaff2da828eec8ef88f546c83d
Clément Lagier <clement.lagier@epitech.eu>
2025-04-03 15:45:06 +0900

    [ADD] Added a documentation to the ServerLogging class

--- lib/server_logging/include/Logging.hpp
+++ lib/server_logging/include/Logging.hpp
@@ -4,9 +4,19 @@
 #include <string>
 #include <ctime>
 
+/**
+* @brief Logging class for logging messages with timestamps.
+*
+* This class provides static methods to log messages with different severity levels
+* (info, error, warning) along with a timestamp.
+*/
 class Logging
 {
   public:
+      /**
+      * @brief Logs an info message with a timestamp.
+      * @param message The message to log.
+      */
       static void Log(const std::string& message)
       {
         time_t now = time(0);
@@ -18,6 +28,10 @@ class Logging
         std::cout << "\033[1;34m" << "[" << dateTime << "]: " << "\033[0m" << message << std::endl;
       }
 
+      /**
+      * @brief Logs an error message with a timestamp.
+      * @param message The message to log.
+      */
       static void LogError(const std::string& message)
       {
         time_t now = time(0);
@@ -29,6 +43,10 @@ class Logging
         std::cout << "\033[1;31m" << "[" << dateTime << "]: " << "\033[0m" << message << std::endl;
       }
 
+      /**
+      * @brief Logs a warning message with a timestamp.
+      * @param message The message to log.
+      */
       static void LogWarning(const std::string& message)
       {
         time_t now = time(0);
@@ -40,5 +58,5 @@ class Logging
         std::cout << "\033[1;33m" << "[" << dateTime << "]: " << "\033[0m" << message << std::endl;
       }
   private:
-      time_t timestamp;
+      time_t timestamp; // Timestamp for logging
 };
